049ea85d4982e864ade0be51c583a45a663c0eb4
sai0566 <sreeram0566@gmail.com>
2026-04-29 21:55:10 +0530

    css fixed

--- studenthub/settings.py
+++ studenthub/settings.py
@@ -128,8 +128,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 AUTH_USER_MODEL='accounts.CustomUser'
